593f77a1a8b3eda31386e126c1a9e5409a8dac37
Robert Shields <robert.shieldsiv@gmail.com>
2020-01-06 21:37:49 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -18,5 +18,21 @@ end
 def add_song(song)
     song.artist = self
 end
+
+def self.find_or_create_by_name(name)
+    self.find(name) ? self.find(name) : self.new(name)
+  end
+
+  def self.find(name)
+    self.all.find {|artist| artist.name == name }
+  end
+
+  def save
+    @@all << self
+  end
+
+  def print_songs
+    songs.each {|song| puts song.name}
+  end
   
 end 
